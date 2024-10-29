3589d51e0ac380b2b2a745b9803976bd07e6767e
JJWayne110 <sanchez.wayne110@gmail.com>
2024-10-28 20:17:34 -0400

    Se agrega el Proyecto

---
+++ Ejercicio 5 (Cambio de $).py
@@ -0,0 +1,14 @@
+def main():
+    print("Indique numero entero positivo mayor que cero correspondiente a la cantidad de dolares")
+    Cd=int(input())
+    CO=37.65
+    MP=47.55
+    EBCO=(Cd*CO)
+    EBMP=(Cd*MP)
+    print("La cantidad de dolares suministrada es: ", Cd)
+    print("De acuerdo al BCV el cambio oficial del dolar es: ", CO,"bs por $")
+    print("El equivalente en bs al Cambio Oficial es: ", EBCO,"bs")
+    print("El cambio del dolar segun el mercado paralelo es: ", MP,"bs por $")
+    print("El equivalente en bs al Mercado Paralelo es: ", EBMP,"bs")
+
+main()
